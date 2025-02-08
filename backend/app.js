b689f3c3dd10131ca0f3ae814147f8ab1c13501e
TheAlienSeb <Sebastianramos5387@gmail.com>
2025-02-08 14:36:59 -0500

    starting to create user middleware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const db = require('./firebase'); // Firestore instance
+const db = require('./server/firebase'); // Firestore instance
 const cors = require('cors')
 
 const app = express()
