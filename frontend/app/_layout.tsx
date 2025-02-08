e227f81b8358b05e2daabbb61ea4138f18882877
Haasil Pujara <haasil@MacBook-Pro.local>
2025-02-08 16:34:49 -0500

    restructured files

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,18 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import {useFonts} from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+import 'react-native-reanimated';
 
-export default function RootLayout() {
-  return <Stack />;
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout()
+{
+
+return (
+  <Stack>
+    <Stack.Screen name="index" options={ { headerShown: false}}/>
+  </Stack>
+)
 }
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { View, Text } from 'react-native';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+export default function Home () {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+};
+
